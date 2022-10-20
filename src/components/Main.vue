2668a34805cc9fd74eaaa27425660e52028acd6e
carladizon <carl@rennga.com>
2022-10-20 23:50:56 +0800

    Added my project to Git

---
+++ src/components/Main.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container row">
+      <div class="left col-md">
+        <span class="headers">Cities</span>
+        <draggable v-model="bucket_1" transition="200" class="drop-zone">
+          <template v-slot:item="{ item }">
+            <div class="draggable-item">
+              {{ item.city }}
+            </div>
+          </template>
+        </draggable>
+      </div>
+      <div class="middle col-md">
+        <span class="headers">United States</span>
+        <draggable v-model="bucket_2" transition="200" class="drop-zone bucket-2" :class="usError ? 'danger' : ''">
+          <template v-slot:item="{ item }">
+            <div class="draggable-item">
+              {{ item.city }}
+            </div>
+          </template>
+        </draggable>
+      </div>
+      <div class="right col-md">
+        <span class="headers">Philippines</span>
+        <draggable v-model="bucket_3" transition="200" class="drop-zone bucket-3" :class="phError ? 'danger' : ''">
+          <template v-slot:item="{ item }">
+            <div class="draggable-item">
+              {{ item.city }}
+            </div>
+          </template>
+        </draggable>
+      </div>
+     </div>
+</template>
+<script>
+import Draggable from "vue3-draggable";
+export default {
+  name: "Main",
+  components: {
+    Draggable,
+  },
+  data() {
+    return {    
+      showError: false,      
+      bucket_1: [{
+        country: 'US', 
+        city: 'New York'
+      },
+      {
+        country: 'US', 
+        city: 'Los Angeles'
+      },
+      {
+        country: 'US', 
+        city: 'Chicago'
+      },
+      {
+        country: 'PH', 
+        city: 'Manila'
+      },
+      {
+        country: 'PH', 
+        city: 'Cebu'
+      },
+      {
+        country: 'PH', 
+        city: 'Dumaguete'
+      },
+    ],
+      bucket_2: [],
+      bucket_3: [],
+    };
+  },
+  watch: {
+    bucket_2(data) {
+      this.usError = false;
+      Object.values(data).forEach((value)=> {
+        this.usError = value.country !== 'US';          
+      });
+    },
+    bucket_3(data) {
+      this.phError = false;
+      
+      Object.values(data).forEach((value)=> {
+        console.log(value)
+        this.phError = value.country !== 'PH';          
+      });
+    }
+  }
+};
+</script>
